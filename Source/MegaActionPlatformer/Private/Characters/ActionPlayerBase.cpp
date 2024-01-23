5dc8040bd56981675798604608ae4062e025b216
Gyeong-rok Min <12equal34@gmail.com>
2024-01-23 23:14:21 +0900

    converts player input methods from blueprint version to native version.

--- Source/MegaActionPlatformer/Private/Characters/ActionPlayerBase.cpp
+++ Source/MegaActionPlatformer/Private/Characters/ActionPlayerBase.cpp
@@ -4,6 +4,16 @@
 #include "Characters/ActionPlayerBase.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+
+DEFINE_LOG_CATEGORY(LogActionPlayerInput);
+
+#pragma warning(disable: 26813)
+
+#define UE_CONVERT_TO_TEXT(a) #a
 
 AActionPlayerBase::AActionPlayerBase()
 {
@@ -17,4 +27,81 @@ AActionPlayerBase::AActionPlayerBase()
 	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	check(CameraComponent);
 	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext> DefaultIMC_Ref(TEXT("/Game/MegaActionPlatformer/Input/IMC_Action.IMC_Action"));
+	check(DefaultIMC_Ref.Succeeded());
+	DefaultIMC = DefaultIMC_Ref.Object;
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> IA_Move_Ref(TEXT("/Game/MegaActionPlatformer/Input/Actions/IA_Move.IA_Move"));
+	check(IA_Move_Ref.Succeeded());
+	IA_Move = IA_Move_Ref.Object;
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> IA_Jump_Ref(TEXT("/Game/MegaActionPlatformer/Input/Actions/IA_Jump.IA_Jump"));
+	check(IA_Jump_Ref.Succeeded());
+	IA_Jump = IA_Jump_Ref.Object;
+}
+
+void AActionPlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (ensure(EnhancedInput))
+	{
+		checkf(IA_Move, TEXT("%s is NOT set."), *IA_Move.GetName());
+		checkf(IA_Jump, TEXT("%s is NOT set."), *IA_Jump.GetName());
+
+		EnhancedInput->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AActionPlayerBase::OnIA_Move);
+		EnhancedInput->BindAction(IA_Jump, ETriggerEvent::Triggered, this, &AActionPlayerBase::OnIA_Jump);
+		EnhancedInput->BindAction(IA_Jump, ETriggerEvent::Completed, this, &AActionPlayerBase::OnIA_Jump);
+		EnhancedInput->BindAction(IA_Jump, ETriggerEvent::Canceled,  this, &AActionPlayerBase::OnIA_Jump);
+	}
+}
+
+void AActionPlayerBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsPlayerControlled())
+	{
+		AddDefaultInputMappingContext();
+	}
+}
+
+void AActionPlayerBase::OnIA_Move(const FInputActionValue& Value)
+{
+	float InputScale = Value.Get<float>();
+	AddMovementInput(FVector::XAxisVector, InputScale);
+	UE_LOG(LogActionPlayerInput,Display, TEXT("Move: %s"), ( InputScale>0.f ? TEXT("Right") : (InputScale<0.f ? TEXT("Left") : TEXT("Zero")) ));
+}
+
+void AActionPlayerBase::OnIA_Jump(const FInputActionInstance& Instance)
+{
+	ETriggerEvent TriggerEvent = Instance.GetTriggerEvent();
+	if (TriggerEvent == ETriggerEvent::Completed)
+	{
+		UE_LOG(LogActionPlayerInput, Display, TEXT("Jump Completed"));
+	}
+	else if (TriggerEvent == ETriggerEvent::Canceled)
+	{
+		UE_LOG(LogActionPlayerInput, Display, TEXT("Jump Canceled"));
+
+		StopJumping();
+	}
+	else if (TriggerEvent == ETriggerEvent::Triggered)
+	{
+		UE_LOG(LogActionPlayerInput, Display, TEXT("Jump Triggered"));
+
+		Jump();
+	}
+}
+
+void AActionPlayerBase::AddDefaultInputMappingContext()
+{
+	APlayerController* PC = CastChecked<APlayerController>(GetController());
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+	check(Subsystem);
+
+	checkf(DefaultIMC, TEXT("%s does NOT setup a property of AActionPlayerBase: " UE_CONVERT_TO_TEXT(DefaultIMC)), *GetName());
+	Subsystem->AddMappingContext(DefaultIMC, 0);
 }
--- Source/MegaActionPlatformer/Public/Characters/ActionPlayerBase.h
+++ Source/MegaActionPlatformer/Public/Characters/ActionPlayerBase.h
@@ -6,6 +6,12 @@
 #include "Characters/ActionCharBase.h"
 #include "ActionPlayerBase.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogActionPlayerInput, Display, All);
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+struct FInputActionInstance;
 
 UCLASS()
 class MEGAACTIONPLATFORMER_API AActionPlayerBase : public AActionCharBase
@@ -14,10 +20,32 @@ class MEGAACTIONPLATFORMER_API AActionPlayerBase : public AActionCharBase
 public:
 	AActionPlayerBase();
 
+	//~ Begin APawn Interface.
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	//~ End APawn Interface.
+protected:
+	//~ Begin AActor Interface.
+	virtual void BeginPlay() override;
+	//~ End AActor Interface.
+
 private:
 	UPROPERTY(Category=Camera,VisibleAnywhere)
 	TObjectPtr<class USpringArmComponent> SpringArmComponent;
 
 	UPROPERTY(Category=Camera,VisibleAnywhere)
 	TObjectPtr<class UCameraComponent> CameraComponent;
+
+private:
+	void OnIA_Move(const FInputActionValue& Value);
+	void OnIA_Jump(const FInputActionInstance& Instance);
+	void AddDefaultInputMappingContext();
+
+	UPROPERTY(Category=Input,EditDefaultsOnly)
+	TObjectPtr<UInputMappingContext> DefaultIMC;
+
+	UPROPERTY(Category=Input,EditDefaultsOnly)
+	TObjectPtr<UInputAction> IA_Move;
+
+	UPROPERTY(Category=Input,EditDefaultsOnly)
+	TObjectPtr<UInputAction> IA_Jump;
 };
